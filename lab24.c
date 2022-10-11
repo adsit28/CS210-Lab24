1d67b6ea8e89c75f3b2ad416ee75e023066c34c7
Adrian de Freitas <Adefreit@users.noreply.github.com>
2022-10-10 23:51:26 -0600

    Uploading Template

---
+++ lab24.c
@@ -0,0 +1,41 @@
+/* lab24.c
+ * =============================================================
+ *    Name:
+ * Section:
+ * Purpose: Practice using malloc / free
+ * =============================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include "lab24functs.h"
+
+int main() {
+
+    // FOLLOW THE LAB INSTRUCTIONS BEFORE WRITING CODE!
+    // Call getNumRecs() with the appropriate parameters
+    // Store the result in a variable
+
+
+    // Print the Number of Records in the File (according to getNumRecs)
+
+
+    // Using the value obtained above, dynamically allocate an array of CadetInfoStructType that is exactly the right size
+    // to hold the number of records in the data file
+
+
+    // Call getDataText() to read the file data into the dynamically allocated array
+
+
+    // Print the First Cadet
+
+
+    // Print the Last Cadet
+
+
+    // Free the array
+
+
+    return 0;
+
+}
