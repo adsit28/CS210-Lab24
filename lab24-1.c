678843e1ccd88e26d47228061a7fd60e24a92e9d
Adrian de Freitas <Adefreit@users.noreply.github.com>
2022-10-15 22:04:12 -0600

    Added new lab

---
+++ lab24-1.c
@@ -0,0 +1,43 @@
+/* lab24-1.c
+ * =============================================================
+ *    Name:
+ * Section:
+ * Purpose: Practice using malloc / free
+ * =============================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+
+    // Step 1:  Ask the user how many PFT scores to process
+
+
+    // Step 2:  Create an array with the exact size needed to store the PFT scores
+    //          Hint:  Use malloc()
+
+
+    // Step 3:  Get all the PFT Scores from the User
+    //          (Store them in your array)
+
+
+    // Step 4:  Calculate the Average of the PFT Scores
+    //          Hint:  Don't forget that the average can be a double!
+
+
+    // Step 5:  Print the Average
+
+
+    // Step 6:  Reexamine All PFT Scores, and Count How Many are Lower than the Average
+
+
+    // Step 7:  Print the # of Cadets Lower than the Average
+
+
+    // Step 8:  Free the Memory you malloc'ed in Step 2
+    //          Hint:  Use free()  
+
+    return 0;
+
+}
